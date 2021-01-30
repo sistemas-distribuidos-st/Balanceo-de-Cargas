80d74a42d702a27984d7d29b0983cf1abbeb4280
AFelipeCR <andres.chaparro06@uptc.edu.co>
2021-01-29 23:27:08 -0500

    Middleware

--- Middleware/index.js
+++ Middleware/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const exec = require('child_process').exec;
 const readLastLines = require('read-last-lines')
 const cors = require('cors')
 const axios = require('axios')
@@ -6,7 +7,7 @@ const { stdout } = require("process")
 const fs = require('fs')
 const app = express()
 const port = 5000
-const ip = 'http://192.168.0.26'
+const ip = '192.168.0.26'
 var currentPort = 49000
 
 app.use(express.static(__dirname + '/public'))
@@ -14,37 +15,49 @@ app.use(cors({ origin: true, credentials: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+
 var servers = [
 ];
 
 let serverAStatus = false
 let time = ''
+addServer();
 
 app.post("/uploadData",(req, res) => {
+    let server = servers.shift();
     console.log("Connection by client");
-    let serverURL = ip + ':' + servers[0].port;
-    console.log("Selected server", serverURL);
 
-    axios({
-        method: 'post',
-        url: serverURL + '/receiveImage',
-        data: req.body.image
-    }).then(imgRes=>{
-        let data = imgRes.data;
-        console.log("Image returns", data.ok);
-        
-        if(data.ok)
-            data.imgURL = serverURL + '/' + req.body.image.name;
+    if(server){
+        let serverURL = 'http://' + ip + ':' + server.port;
+        console.log("Selected server", serverURL);
 
-         res.send(data)
-        
-    }).catch(err=>{
-        console.log(err);
-    });
+        axios({
+            method: 'post',
+            url: serverURL + '/receiveImage',
+            data: req.body.image
+        }).then(imgRes=>{
+            let data = imgRes.data;
+            console.log("Image returns", data.ok);
+            
+            if(data.ok){
+                data.imgURL = serverURL + '/' + req.body.image.name;
+            }else{
+
+            }
+            servers.push(server);
+            res.send(data)
+        }).catch(err=>{
+            console.log(err);
+        });
+    }else{
+        console.log("Cancele");
+    }
+    
 });
 
 
 app.get("/add_server", (req, res) => {
+    addServer();
     res.send("Añadir")
     //exec()
     //servers.push()
@@ -55,13 +68,22 @@ var serversInfo = [];
 setInterval(() => {
     serversInfo = [];
 
+    servers.forEach(ss=>{
+        exec(`sh watch.sh ${ip} ${ss.port}`, (error, stout, stderr) => {
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        })
+    });
+
     readLastLines.read('log.txt', servers.length).then((text) => {
         let lines = text.split('\n');
         lines.splice(lines.length - 1)
         let data;
+
         for (var i = 0; i < lines.length; i++) {
            data = lines[i].split(' ');
-           serversInfo.push({time:data[0], ip:'', status:data[2] ==="OK"})
+           serversInfo.push({time:data[0], port:data[1], status:data[2] == "Server"})
         }
     });
 }, 1000);
@@ -74,23 +96,25 @@ app.get("/logs", (req, res) => {
     res.sendFile(__dirname + '/index.html')
 });
 
-app.get("/restart", (req, res) => {
-    const exec = require('child_process').exec;
-    
-    if (serverAStatus == 'FAIL') {
-        exec('sh restart.sh 192.168.1.14', (error, stout, stderr) => {
-            console.log(`${stdout}`);
-            console.log(`${stderr}`);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        })
-    }
-});
-
 app.listen(port, () => {
     console.log(`App is listening to port ${port}`);
 });
+
+function addServer(){
+    let port = currentPort++;
+    console.log(`sh add-server.sh felipe root ${ip} ${port}`);
+
+    exec(`sh add-server.sh felipe root ${ip} ${port}`, (error, stout, stderr) => {
+        console.log(`${stdout}`);
+        console.log(`${stderr}`);
+        
+        if (error !== null) {
+            console.log(`exec error: ${error}`);
+        }else{
+            servers.push({port: port})
+        }
+    })
+}
 
 function base64_decode(data) {
     return new Buffer(data, 'base64');
@@ -98,18 +122,4 @@ function base64_decode(data) {
 
 function base64_encode(file) {
     return new Buffer(fs.readFileSync(file)).toString('base64');
-}
-
-function addServer(){
-    let port = currentPort++;
-     exec(`sh add-server.sh felipe root ${ip} ${port}`, (error, stout, stderr) => {
-        console.log(`${stdout}`);
-        console.log(`${stderr}`);
-        
-        if (error !== null) {
-            console.log(`exec error: ${error}`);
-        }
-    })
-
-    servers.push({ port: port})
 }
